7be16b813b67575e8b19a5f0ceeee3542a8b5f88
mevlanaayas <mevlanaayas@gmail.com>
2019-06-24 14:15:49 +0300

    fixed: Count function doesnt return an error

--- models/common.go
+++ models/common.go
@@ -1,12 +1,6 @@
 package models
 
-import "fmt"
-
 func Count(tableName string) (count int) {
-	err := GetDB().Table(tableName).Count(&count)
-	if err != nil {
-		fmt.Println(err)
-		return 0
-	}
+	GetDB().Table(tableName).Count(&count)
 	return
 }
